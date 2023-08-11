28e83a89855ca6ef9b1a63ce849d47acd4c212c2
Michael <addictedchrysler@gmail.com>
2023-08-10 17:25:12 -0700

    task 0 beautiful code

--- 0-beauty.c
+++ 0-beauty.c
@@ -1,42 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 long long int product(long int a, long int b);
 
+/**
+ * main - Multiply two numbers.
+ *
+ * Return: The product of a and b.
+ */
+
+
 int main(void)
-{	long int x, y;
+{
+	long int x, y;
 	long long int prod;
 	char check;
 
+	/* Prompt user for input */
 	printf("I will multiply any two numbers you enter.\n");
 	printf("Enter (Y) to proceed, (N) to quit.\n");
-	scanf("%c",&check);
-	if ( check == 'N' || check == 'n' )
-	{	printf("Okay, have a nice day\n");
+	scanf("%c", &check);
+
+	/* Check user's choice */
+	if (check == 'N' || check == 'n')
+	{
+		printf("Okay, have a nice day\n");
 		exit(0);
 	}
-	else if (check == 'Y'|| check == 'y')
-	{	
+	else if (check == 'Y' || check == 'y')
+	{
+		/* Get user input for numbers */
 		printf("Enter first Number: ");
 		scanf("%ld", &x);
 		printf("Enter second Number: ");
 		scanf("%ld", &y);
 
-		prod = product(x , y);
-		printf("The product is %lld \n", prod);
+		/* Calculate and display product */
+		prod = product(x, y);
+		printf("The product is %lld\n", prod);
 	}
 	else
 	{
+		/* Invalid input */
 		printf("Invalid input\n");
 		printf("Okay Bye.\n");
 	}
 
-	return(0);	
+	return (0);
 }
 
+/**
+ * product - Multiply two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: The product of a and b.
+ */
 long long int product(long int a, long int b)
 {
-	long long int mult;
-
-	mult = a * b;
-	return (mult);
+	/* Calculate and return the product */
+	return (a * b);
 }
+
