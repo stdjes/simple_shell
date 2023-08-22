b9351db0151cf7ab918bd0a2bb5a7665e09564d7
Michael <addictedchrysler@gmail.com>
2023-08-21 17:54:11 -0700

    env function

---
+++ env.c
@@ -0,0 +1,15 @@
+#include "ssh.h"
+
+/**
+ * bin_env - Coding for the env builtin
+ *
+ * @info: info about shell
+ * @arguments: Commands or arguments
+ **/
+void bin_env(general_t *info, char **arguments)
+{
+	(void) info;
+	(void) arguments;
+
+	get_full_env();
+}
