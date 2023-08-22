c98878b8bcc100876c6e865352a091979c7116dd
stdjes <stephendjes@gmail.com>
2023-08-21 19:44:03 -0700

    start command prompt entry

---
+++ run.c
@@ -0,0 +1,20 @@
+#include "ssh.h"
+
+/**
+ * start - Handle the mode of the shell.
+ *
+ * Description:
+ * This function handles the mode of the shell,
+ * which can be either INTERACTIVE or NON_INTERACTIVE.
+ *
+ * @info: Struct containing information about the shell.
+ *
+ * This function invokes the start_prompt function
+ * to initiate the shell's prompt
+ * and interaction based on the mode specified in the @info struct.
+ **/
+void start(general_t *info)
+{
+	/* Initiate the appropriate shell prompt based on the mode */
+    start_prompt(info);
+}
