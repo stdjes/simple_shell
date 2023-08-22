476456b2af2cb5557b8dbb9cb3e5675c560f2274
Michael <addictedchrysler@gmail.com>
2023-08-21 18:50:05 -0700

    main shell file

---
+++ shell.c
@@ -0,0 +1,36 @@
+#include "ssh.h"
+
+/**
+ * main - The entry point of the shell
+ *
+ * @argc: Number of arguments
+ * @argv: Arguments
+ *
+ * Return: 0 on success and 1 on error
+ **/
+int main(int argc, char **argv)
+{
+	general_t *info;
+	int status_code;
+
+	info = malloc(sizeof(general_t));
+	if (info == NULL)
+	{
+		perror(argv[0]);
+		exit(1);
+	}
+
+	info->pid = getpid();
+	info->status_code = 0;
+	info->n_commands = 0;
+	info->argc = argc;
+	info->argv = argv;
+	info->mode = isatty(STDIN) == INTERACTIVE;
+	start(info);
+
+	status_code = info->status_code;
+
+	free(info);
+
+	return (status_code);
+}
