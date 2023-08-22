643acf036c05739c5158072e42a4164fa65b5623
stdjes <stephendjes@gmail.com>
2023-08-21 17:48:36 -0700

    fixing up betty issues

--- cmd_args.c
+++ cmd_args.c
@@ -22,11 +22,9 @@ void analyze(char **arguments, general_t *info, char *buff)
 
 	cmd = arguments[0];
 	info->command = cmd;
-
 	/* Check if the command is a built-in command */
 	if (check_builtin(info, arguments) == _TRUE)
 		return;
-
 	/* Check if the command is a file and has execute permissions */
 	status = is_file(cmd);
 	if (status == NON_PERMISSIONS)
@@ -36,17 +34,14 @@ void analyze(char **arguments, general_t *info, char *buff)
 		error(info);
 		return;
 	}
-
 	if (status == 1)
 	{
 		execute(cmd, arguments, info, buff);
 		return;
 	}
-
 	/* Check if the command is in the current directory */
 	if (current_dir(cmd, arguments, buff, info) == _TRUE)
 		return;
-
 	/* Look for the command in the PATH and execute if found */
 	info->value_path = which(cmd, info);
 	if (info->value_path != NULL)
@@ -55,7 +50,6 @@ void analyze(char **arguments, general_t *info, char *buff)
 		free_memory_p((void *) info->value_path);
 		return;
 	}
-
 	/* Command not found */
 	info->status_code = 127;
 	info->error_code = _CODE_CMD_NOT_EXISTS;
