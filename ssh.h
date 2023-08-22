222870f6fe2e4b50f5082be6598d142e17d6c4db
Michael <addictedchrysler@gmail.com>
2023-08-21 17:06:45 -0700

    file prototypes

--- ssh.h
+++ ssh.h
@@ -1,6 +1,32 @@
 #ifndef _SSH_H_
 #define _SSH_H_
 
+#define _TRUE            1
+#define _FALSE           0
+
+#define STDIN            0
+#define STDOUT           1
+#define STDERR           2
+
+#define NON_INTERACTIVE  0
+#define INTERACTIVE      1
+
+#define PERMISSIONS      1
+#define NON_PERMISSIONS -1
+
+#define _FILE            10
+#define NON_FILE         -10
+
+#define _ENOENT          "No such file or directory"
+#define _EACCES          "Permission denied"
+#define _CMD_NOT_EXISTS  "not found"
+#define _ILLEGAL_NUMBER  "Illegal number"
+
+#define _CODE_ENOENT           3
+#define _CODE_EACCES           13
+#define _CODE_CMD_NOT_EXISTS   132
+#define _CODE_ILLEGAL_NUMBER   133
+
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -41,6 +67,37 @@ void get_full_env(void);
 /* execution fucntions */
 void execute(char *commands, char **arguments, general_t *info, char *buff);
 int current_dir(char *cmd, char **arguments, char *buff, general_t *info);
+
+typedef struct __attribute__((__packed__))
+{
+	int argc;                 /* Number of arguments received */
+	char **argv;              /* Arguments received */
+	int mode;                 /* INTERACTIVE or NON_INTERACTIVE */
+	int error_code;           /* Error code for error message */
+	char *command;            /* Command to execute */
+	int n_commands;           /* Number of commands executed */
+	char *value_path;         /* Path of a command */
+	int is_current_path;      /* Check if is current path or not */
+	int status_code;          /* Last exit code */
+	char *buffer;             /* Line readed with the getline */
+	char **arguments;         /* Line splited into words */
+	char *environment;        /* Last environment variable get it */
+	int pid;                  /* Process id */
+} general_t;
+
+typedef struct __attribute__((__packed__))
+{
+	char *message;            /* Error message */
+	int code;                 /* Error code for identify the error message */
+} error_t;
+
+typedef struct __attribute__((__packed__))
+{
+	char *command;            /* arguments[0] = cmd */
+	void (*func)(general_t *info, char **arguments);
+} builtin_t;
+
+
 
 
 #endif _SSH_H_  /* close guard */
