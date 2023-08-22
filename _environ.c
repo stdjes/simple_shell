23f4957c63258a272d0d62006ffa832c62170775
stdjes <stephendjes@gmail.com>
2023-08-21 18:05:06 -0700

    fixing all betty issues on _environ.c

--- _environ.c
+++ _environ.c
@@ -11,42 +11,42 @@
  **/
 char *_getenv(const char *name)
 {
-    char **env;
-    char *aux, *token, *value;
-    int size;
+	char **env;
+	char *aux, *token, *value;
+	int size;
 
-    size = _strlen((char *)name);
+	size = _strlen((char *)name);
 
-    for (env = environ; *env; ++env)
-    {
-        aux = _strdup(*env);
+	for (env = environ; *env; ++env)
+	{
+		aux = _strdup(*env);
 
-        token = strtok(aux, "=");
-        if (token == NULL)
-        {
-            free(aux);
-            return (NULL);
-        }
+		token = strtok(aux, "=");
+		if (token == NULL)
+		{
+			free(aux);
+			return (NULL);
+		}
 
-        if (_strlen(token) != size)
-        {
-            free(aux);
-            continue;
-        }
+		if (_strlen(token) != size)
+		{
+			free(aux);
+			continue;
+		}
 
-        if (_strcmp((char *)name, aux) == 0)
-        {
-            token = strtok(NULL, "=");
-            value = _strdup(token);
+		if (_strcmp((char *)name, aux) == 0)
+		{
+			token = strtok(NULL, "=");
+			value = _strdup(token);
 
-            free(aux);
-            return (value);
-        }
+			free(aux);
+			return (value);
+		}
 
-        free(aux);
-    }
+		free(aux);
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
 /**
@@ -60,44 +60,44 @@ char *_getenv(const char *name)
  */
 char *which(char *filename, general_t *info)
 {
-    char *path, *tmp_path, *token;
-    char *slash;
-    int size;
+	char *path, *tmp_path, *token;
+	char *slash;
+	int size;
 
-    (void)info; /* Unused parameter */
+	(void)info; /* Unused parameter */
 
-    path = _getenv("PATH");
-    if (path == NULL)
-        return (NULL);
+	path = _getenv("PATH");
+	if (path == NULL)
+		return (NULL);
 
-    token = strtok(path, ":");
+	token = strtok(path, ":");
 
-    size = _strlen(filename) + 2;
-    slash = malloc(size * sizeof(char));
-    slash = _strcpy(slash, "/");
-    slash = _strcat(slash, filename);
+	size = _strlen(filename) + 2;
+	slash = malloc(size * sizeof(char));
+	slash = _strcpy(slash, "/");
+	slash = _strcat(slash, filename);
 
-    while (token != NULL)
-    {
-        tmp_path = malloc(_strlen(token) + size);
-        tmp_path = _strcpy(tmp_path, token);
-        tmp_path = _strcat(tmp_path, slash);
+	while (token != NULL)
+	{
+		tmp_path = malloc(_strlen(token) + size);
+		tmp_path = _strcpy(tmp_path, token);
+		tmp_path = _strcat(tmp_path, slash);
 
-        if (is_executable(tmp_path) == PERMISSIONS)
-        {
-            free(slash);
-            free(path);
-            return (tmp_path);
-        }
-        token = strtok(NULL, ":");
+		if (is_executable(tmp_path) == PERMISSIONS)
+		{
+			free(slash);
+			free(path);
+			return (tmp_path);
+		}
+		token = strtok(NULL, ":");
 
-        free(tmp_path);
-    }
+		free(tmp_path);
+	}
 
-    free(path);
-    free(slash);
+	free(path);
+	free(slash);
 
-    return (NULL);
+	return (NULL);
 }
 
 /**
@@ -108,13 +108,13 @@ char *which(char *filename, general_t *info)
  */
 void is_current_path(char *path, general_t *info)
 {
-    info->is_current_path = _FALSE;
+	info->is_current_path = _FALSE;
 
-    if (path == NULL)
-        return;
+	if (path == NULL)
+		return;
 
-    if (path[0] == ':')
-        info->is_current_path = _TRUE;
+	if (path[0] == ':')
+		info->is_current_path = _TRUE;
 }
 
 /**
@@ -122,12 +122,12 @@ void is_current_path(char *path, general_t *info)
  */
 void get_full_env(void)
 {
-    char **tmp;
-    int i;
+	char **tmp;
+	int i;
 
-    for (i = 0, tmp = environ; tmp[i] != NULL; i++)
-    {
-        print(tmp[i]); /* Assuming print() function displays text */
-        _putchar('\n');
-    }
+	for (i = 0, tmp = environ; tmp[i] != NULL; i++)
+	{
+		print(tmp[i]); /* Assuming print() function displays text */
+		_putchar('\n');
+	}
 }
