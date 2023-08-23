fe04796ab94fae8ed4e2fb8f13938ef31b8f27b4
stdjes <stephendjes@gmail.com>
2023-08-23 16:20:42 -0700

    beauty code push

---
+++ 0-beauty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * product - Multiply two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: The product of a and b.
+ */
+long long int product(long int a, long int b)
+{
+	/* Calculate and return the product */
+	return (a * b);
+}
+
